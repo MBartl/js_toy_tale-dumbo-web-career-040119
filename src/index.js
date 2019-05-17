b2e360554f74bd46f08d35c7c962e38dbaa02364
github username <github email address>
2019-05-17 11:41:46 -0400

    deliverables 1-3 done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ const toyForm = document.querySelector('.container')
 let addToy = false
 
 // YOUR CODE HERE
+const toyList = document.querySelector('#toy-collection')
+
+fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(data => data.forEach(slapItOnTheList))
+
+function slapItOnTheList(toy) {
+  const div = document.createElement('div');
+  div.innerHTML = `<h2>${toy.name}</h2>
+  <img src=${toy.image} alt='${toy.name} picture' />
+  <p>${toy.likes} likes</p>
+  <button class='like-btn'>Like <3</button>`;
+  toyList.appendChild(div);
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
